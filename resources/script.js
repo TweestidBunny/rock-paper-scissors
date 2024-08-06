4ff434e5aedb4edaede8a83f4bacb5e8e91f591f
Sean <tweestidbunny@gmail.com>
2024-08-05 21:06:43 -0400

    Change playerWin/computerWin to playerScore/computerScore

--- resources/script.js
+++ resources/script.js
@@ -9,28 +9,28 @@ let getComputerChoice = () => +(Math.random() * 2).toFixed(0);
 // console.log(getComputerChoice())
 
 // Create win counter for Player
-let playerWin = 0;
+let playerScore = 0;
 
 // Create win counter for Computer
-let computerWin = 0;
+let computerScore = 0;
 
 // Compare player choice and computer choice.
 function compareChoice(pChoice, cChoice) {
   if (pChoice === 0) {
-    cChoice === 1 ? computerWin++ : cChoice === 2 ? playerWin++ : console.log('Tie');
+    cChoice === 1 ? computerScore++ : cChoice === 2 ? playerScore++ : console.log('Tie');
   } else if (pChoice === 1) {
-    cChoice === 0 ? playerWin++ : cChoice === 2 ? computerWin++ : console.log('Tie');
+    cChoice === 0 ? playerScore++ : cChoice === 2 ? computerScore++ : console.log('Tie');
   } else if (pChoice === 2) {
-    cChoice === 0 ? computerWin++ : cChoice === 1 ? playerWin++ : console.log('Tie');
+    cChoice === 0 ? computerScore++ : cChoice === 1 ? playerScore++ : console.log('Tie');
   }
 }
 
 // Create game loop.
-while (playerWin < 5 && computerWin < 5) {
+while (playerScore < 5 && computerScore < 5) {
   compareChoice(getPlayerChoice(), getComputerChoice());
-  console.log(`Player Wins: ${playerWin}`);
-  console.log(`Computer Wins: ${computerWin}`);
+  console.log(`Player Wins: ${playerScore}`);
+  console.log(`Computer Wins: ${computerScore}`);
 }
 
 // console.log winner.
-console.log(playerWin > computerWin ? 'Player Wins!' : 'Computer Wins!')
+console.log(playerScore > computerScore ? 'Player Wins!' : 'Computer Wins!')
